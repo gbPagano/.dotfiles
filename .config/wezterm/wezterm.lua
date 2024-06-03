e8518a69967422c325b8f2d62a2a26cd6798c5ec
gbPagano <guilhermebpagano@gmail.com>
2024-06-02 23:37:34 -0300

    update starship and wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font 'Roboto Mono Nerd Font Mono'
+
 config.color_scheme = 'Catppuccin Mocha'
-config.font_size = 18.0
+config.font_size = 14.0
 
 config.scrollback_lines = 3500
 config.hide_tab_bar_if_only_one_tab = true
