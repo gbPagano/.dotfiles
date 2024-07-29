161c4f622af60003e3c37e57bb2fba486c096ec5
gbPagano <guilhermebpagano@gmail.com>
2024-07-28 22:12:25 -0300

    feat(fzf): add toggle preview on tab completion
    alt-p to toggle preview

--- .config/fzf/fzf.sh
+++ .config/fzf/fzf.sh
@@ -1,23 +1,26 @@
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--preview-window=hidden \
+--bind 'alt-p:toggle-preview'"
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS="--height 20%"
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,.venv,target
   --preview '~/.config/fzf/fzf-preview.sh {}'
-  --bind 'ctrl-/:change-preview-window(hidden|)'
+  --preview-window=nohidden
   --height 40%"
 export FZF_ALT_C_OPTS="
   --walker-skip .git,.venv,target
+  --preview-window=nohidden
   --preview 'tree -C {} | head -200'
   --height 20%"
 
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd --color always --icon always $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'lsd --color always --icon always $realpath'
-zstyle ':fzf-tab:complete:code*' fzf-preview '~/.config/fzf/fzf-preview.sh $realpath'
+zstyle ':fzf-tab:complete:*' fzf-preview '~/.config/fzf/fzf-preview.sh $realpath'
 
 eval "$(fzf --zsh)"
