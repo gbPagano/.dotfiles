9b20c303a2814a23e329a309160ad1ae2c3ba5e8
gbPagano <guilhermebpagano@gmail.com>
2024-08-05 22:20:26 -0300

    feat: add initial neovim config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.cmd("set autoindent")
+vim.cmd("set visualbell")
+vim.cmd("set ignorecase")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
