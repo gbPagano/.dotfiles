e138f9c6aabb1e9d4fa4eab3aee81a04bf073879
gbPagano <guilhermebpagano@gmail.com>
2024-10-15 22:44:38 -0300

    fix: update fzf config for new version

--- config/fzf/fzf.sh
+++ config/fzf/fzf.sh
@@ -1,7 +1,8 @@
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
 --preview-window=hidden \
 --bind 'alt-p:toggle-preview'"
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
@@ -19,6 +20,7 @@ export FZF_ALT_C_OPTS="
   --height 20%"
 
 zstyle ':completion:*' menu no
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd --color always --icon always $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'lsd --color always --icon always $realpath'
 zstyle ':fzf-tab:complete:*' fzf-preview '~/.config/fzf/fzf-preview.sh $realpath'
