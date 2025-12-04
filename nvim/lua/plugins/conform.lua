b13dd0137438dea6624f50048ba2d50ba61b977f
gbPagano <guilhermebpagano@gmail.com>
2025-12-04 15:43:04 -0300

    refactor: reorganize directories

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			-- python = { "isort", "black" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			rust = { "rustfmt" },
+			-- Conform will run the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+	keys = {
+		{
+			"gf",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback", timeout_ms = 500 })
+			end,
+			desc = "Conform Format",
+		},
+	},
+}
