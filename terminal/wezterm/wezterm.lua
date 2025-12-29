0ff95f31db34489d73ac52229b917487d3c97207
gbPagano <guilhermebpagano@gmail.com>
2025-12-29 14:49:40 -0300

    refactor(wezterm): update font

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,17 +1,18 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'Roboto Mono Nerd Font Mono'
+-- config.font = wezterm.font 'Roboto Mono Nerd Font Mono'
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 
-config.color_scheme = 'Catppuccin Mocha'
-config.font_size = 14.0
+config.color_scheme = "Catppuccin Mocha"
+config.font_size = 12.0
 
 config.scrollback_lines = 3500
 config.hide_tab_bar_if_only_one_tab = true
 config.max_fps = 144
 
-config.enable_wayland = false
-config.window_close_confirmation = 'NeverPrompt'
+config.enable_wayland = true
+config.window_close_confirmation = "NeverPrompt"
 
 return config
