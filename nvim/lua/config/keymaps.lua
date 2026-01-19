0baed68b18c639b1fc4aa6c3b5e44039c6144d93
gbPagano <guilhermebpagano@gmail.com>
2026-01-18 21:46:24 -0300

    feat(nvim): add flash search plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -145,3 +145,11 @@ Snacks.toggle({
 })
   :map("<leader>uv")
   :map("<leader>tv")
+-- Flash jumps
+-- stylua: ignore start
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Flash Treesitter Search" })
+vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
+-- stylua: ignore end
